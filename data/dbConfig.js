a93dd6a96bd17641261523812fcbedf017e6f4f1
Chawn Christian <chawn.christian@gmail.com>
2020-02-01 12:07:24 -0500

    MVP + one stretch goal

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,4 +2,40 @@ const knex = require('knex');
 
 const configOptions = require('../knexfile').development;
 
-module.exports = knex(configOptions);
+const db = knex(configOptions)
+/*module.exports = {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+  };
+  
+  function find() {
+    return db('accounts');
+  }
+  
+  function findById(id) {
+    return db('accounts')
+      .where({ id: Number(id) })
+      .first();
+  }
+  
+  function insert(account) {
+    return db('accounts')
+      .insert(account)
+      .then(ids => ({ id: ids[0] }));
+  }
+  
+  function update(id, account) {
+    return db('accouts')
+      .where('id', Number(id))
+      .update(account);
+  }
+  
+  function remove(id) {
+    return db('acounts')
+      .where('id', Number(id))
+      .del();
+  }*/
+  module.exports = db;
--- queries.sql
+++ queries.sql
@@ -1,17 +1,81 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT * FROM Customers
+WHERE Postalcode = 1010;
+12	Cactus Comidas para llevar	Patricio Simpson	Cerrito 333	Buenos Aires	1010	Argentina
+54	Océano Atlántico Ltda.	Yvonne Moncada	Ing. Gustavo Moncada 8585 Piso 20-A	Buenos Aires	1010	Argentina
+64	Rancho grande	Sergio Gutiérrez	Av. del Libertador 900	Buenos Aires	1010	Argentina
+
 
 -- Find the phone number for the supplier with the id 11
+SELECT * FROM Suppliers
+WHERE SupplierID = 11;
+
+PhoneNumber: (010) 9984510
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT * FROM Orders;
+10443	66	8	1997-02-12	1
+10442	20	3	1997-02-11	2
+10442	20	3	1997-02-11	2
+10441	55	3	1997-02-10	2
+10440	71	4	1997-02-10	2
+10439	51	6	1997-02-07	3
+10438	79	3	1997-02-06	2
+10437	87	8	1997-02-05	1
+10436	7	3	1997-02-05	2
+10435	16	8	1997-02-04	2
+10434	24	3	1997-02-03	2
+10433	60	3	1997-02-03	3
+
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT * FROM Customers
+Where City = "London";
+4	Around the Horn	Thomas Hardy	120 Hanover Sq.	London	WA1 1DP	UK
+11	Bs Beverages	Victoria Ashworth	Fauntleroy Circus	London	EC2 5NT	UK
+16	Consolidated Holdings	Elizabeth Brown	Berkeley Gardens 12 Brewery	London	WX1 6LT	UK
+19	Eastern Connection	Ann Devon	35 King George	London	WX3 6FW	UK
+53	North/South	Simon Crowther	South House 300 Queensbridge	London	SW7 1RZ	UK
+72	Seven Seas Imports	Hari Kumar	90 Wadhurst Rd.	London	OX15 4NB	UK
+8	Bólido Comidas preparadas	Martín Sommer	C/ Araquil, 67	Madrid	28023	Spain
+22	FISSA Fabrica Inter. Salchichas S.A.	Diego Roel	C/ Moralzarzal, 86	Madrid	28034	Spain
+69	Romero y tomillo	Alejandra Camino	Gran Vía, 1	Madrid	28001	Spain
+15	Comércio Mineiro	Pedro Afonso	Av. dos Lusíadas, 23	São Paulo	05432-043	Brazil
+21	Familia Arquibaldo	Aria Cruz	Rua Orós, 92	São Paulo	05442-030	Brazil
+31	Gourmet Lanchonetes	André Fonseca	Av. Brasil, 442	Campinas	04876-786	Brazil
+34	Hanari Carnes	Mario Pontes	Rua do Paço, 67	Rio de Janeiro	05454-876	Brazil
+61	Que Delícia	Bernardo Batista	Rua da Panificadora, 12	Rio de Janeiro	02389-673	Brazil
+62	Queen Cozinha	Lúcia Carvalho	Alameda dos Canàrios, 891	São Paulo	05487-020	Brazil
+67	Ricardo Adocicados	Janete Limeira	Av. Copacabana, 267	Rio de Janeiro	02389-890	Brazil
+81	Tradição Hipermercados	Anabela Domingues	Av. Inês de Castro, 414	São Paulo	05634-030	Brazil
+88	Wellington Importadora	Paula Parente	Rua do Mercado, 12	Resende	08737-363	Brazil
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-
+INSERT INTO Customers(CustomerName, ContactName, Address, City, PostalCode, Country) values ("Bilbo Baggins", "1 Hobbit Hole", "Bag End", "111", "Middle Earth" )
+93	The Shire	Bilbo Baggins	1 Hobbit Hole	Bag End	111	Middle Earth
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
-
+UPDATE Customers SET PostalCode= '11122'
+WHERE CustomerID = 93;
+93	The Shire	Bilbo Baggins	1 Hobbit Hole	Bag End	11122	Middle Earth
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+SELECT SupplierName, LENGTH(SupplierName) AS NamesLongerThanTwentyCharacters 
+FROM Suppliers
+WHERE LENGTH(SupplierName) > 20;
+
+Number of Records: 11
+SupplierName	NamesLongerThanTwentyCharacters
+New Orleans Cajun Delights	26
+Grandma Kelly's Homestead	25
+Cooperativa de Quesos 'Las Cabras'	34
+Specialty Biscuits, Ltd.	24
+Refrescos Americanas LTDA	25
+Heli Süßwaren GmbH & Co. KG	31
+Plutzer Lebensmittelgroßmärkte AG	33
+Nord-Ost-Fisch Handelsgesellschaft mbH	38
+Formaggi Fortini s.r.l.	23
+Aux joyeux ecclésiastiques	26
+New England Seafood Cannery	27
